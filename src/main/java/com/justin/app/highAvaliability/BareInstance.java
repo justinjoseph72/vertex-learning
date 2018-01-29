b5b9464d0c7e7665d430621793a6bf265b216a2a
Justin Joseph <justin.joseph72@outlook.com>
2018-01-29 16:12:57 +0000

    adding baseInstance

---
+++ src/main/java/com/justin/app/highAvaliability/BareInstance.java
@@ -0,0 +1,11 @@
+package com.justin.app.highAvaliability;
+
+
+import io.vertx.core.Launcher;
+
+public class BareInstance {
+
+    public static void main(String[] args){
+        Launcher.main(new String[]{"bare"});
+    }
+}
